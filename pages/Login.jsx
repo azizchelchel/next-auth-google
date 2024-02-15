3b6aba47b2acea208fbcbaca30d2ab588fa88a8d
chellal aziz <chellalaziz31@gmail.com>
2024-02-15 09:56:07 +0100

    style(Login): style login and logout messages

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -6,34 +6,45 @@ const Login = () => {
   console.log(session);
   if (session) {
     return (
-      <div>
-        <p>welcome user : {session.user.email}</p>
-        <p>you now signed in</p>
-        <div>
-          <button
-            className="border border-black rounded-lg p-1'"
-            onClick={() => {
-              signOut();
-            }}
-          >
-            Sign Out
-          </button>
+      <div className="w-full flex flex-col bg-slate-700 justify-center items-center h-screen">
+        <div className="w-2/3 h-auto  border-4 rounded-lg p-5 border-green-500">
+          <div className="mb-[4rem] flex flex-col items-center">
+            <p>welcome user : {session.user.email}</p>
+            <p>you are now signed in</p>
+          </div>
+          <div className="flex justify-center">
+            <button
+              className="border text-white border-slate-800  hover:border-slate-300 rounded-lg p-1 bg-sky-600"
+              onClick={() => {
+                signOut();
+              }}
+            >
+              Sign Out
+            </button>
+          </div>
         </div>
       </div>
     );
   } else {
     return (
       <div>
-        <p>you are not signed in</p>
-        <button
-          style={{ borderRadius: "50px" }}
-          className="border border-black bg-slate-600 p-2"
-          onClick={() => {
-            signIn();
-          }}
-        >
-          Sign In
-        </button>
+        <div className="w-full flex flex-col bg-slate-700 justify-center items-center h-screen">
+          <div className="w-2/3 h-auto  border-4 rounded-lg p-5 border-sky-400">
+            <div className="mb-[4rem] flex flex-col items-center">
+              <p>Please, sign in to get access</p>
+            </div>
+            <div className="flex justify-center">
+              <button
+                className="border text-white border-slate-800  hover:border-slate-300 rounded-lg py-1 px-3 bg-sky-600"
+                onClick={() => {
+                  signIn();
+                }}
+              >
+                Sign In
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
